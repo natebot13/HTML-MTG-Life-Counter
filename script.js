51a14b1c82efca300a7cb348f53c9d1c6ad490f8
unknown <christoipher.chen@gmail.com>
2015-08-17 12:56:53 -0700

    more skeleton

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-function xcrement(player, amount){
+function xcrement(player, amount) {
+    document.getElementById(player).getElementById("lifeTotal") += amount
+}
+
+function setGradient(player, colorString) {
+    //(red,green,blue,white,black)
+    var gradStr = ""
+    for (var i = 0; i < colorString.length; i++) {
+        if (colorString[i].toLowerCase == "r") {
+            gradString += "red"
+        } else
+        if (colorString[i].toLowerCase == "g") {
+            gradString += "green"
+        } else
+        if (colorString[i].toLowerCase == "u") {
+            gradString += "blue"
+        } else
+        if (colorString[i].toLowerCase == "w") {
+            gradString += "white"
+        } else
+        if (colorString[i].toLowerCase == "b") {
+            gradString += "black"
+        }
+        if (i != colorString.length) {
+            gradString += ","
+        }
+    }
+    console.log("Set " + player + " background to " + gradString)
+    document.getElementById(player).style.background = "linear-gradient(" + gradString + ");"
 
 }
 
-function setGradient(player, colorString){
+function addColor(player, letter) {
 
 }
 
-function addColor(player, letter){
-
-}
-
-function resetColors(player){
+function resetColors(player) {
 
 }
